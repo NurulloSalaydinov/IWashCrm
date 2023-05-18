8adb8e476747ceb78ac1bffde244e9fdd8809b97
Safarbek <solijonovsafarbek@gmail.com>
2023-05-18 16:13:44 +0500

    new summary

--- box/views.py
+++ box/views.py
@@ -2,9 +2,6 @@ from django.http import JsonResponse
 from django.shortcuts import render
 
 
-
-
-
 def receive_money(request):
     token = request.GET.get('token', None)
     customer_id = request.GET.get('customer_id', None)
