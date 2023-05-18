2a8c5987993acbb3afb95f32f158b72061028984
Dev <86796772+NurulloSalaydinov@users.noreply.github.com>
2023-05-18 15:32:01 +0500

    Update views.py

--- box/views.py
+++ box/views.py
@@ -1,2 +1,12 @@
+from django.http import JsonResponse
 from django.shortcuts import render
 
+
+
+
+
+def receive_money(request):
+    token = request.GET.get('token', None)
+    customer_id = request.GET.get('customer_id', None)
+
+    return JsonResponse({"error": "Something went wrong!"})
